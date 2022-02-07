4cb04e84affa5932109f9e57bf835d66c6f7b3bc
Lalit <lalitvijay9480@gmail.com>
2022-02-07 23:24:43 +0530

    feat: add favicon and title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,10 +3,15 @@ import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'next-themes';
 import { SessionProvider } from 'next-auth/react';
 import { SWRConfig } from 'swr';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <SessionProvider session={session}>
+      <Head>
+        <title>Hyperdocs - Documentation made easy</title>
+        <link rel='shortcut icon' href='/favicon.png' type='image/x-icon' />
+      </Head>
       <SWRConfig
         value={{ fetcher: (url) => fetch(url).then((res) => res.json()) }}>
         <ThemeProvider attribute='class'>
