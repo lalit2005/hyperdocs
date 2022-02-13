1a1f543c1bc5862d0530e5b86f6729f3990c4ca8
Lalit <lalitvijay9480@gmail.com>
2022-02-13 20:54:52 +0530

    refactor: add `DocsLayout` component

---
+++ layouts/DocsLayout.tsx
@@ -0,0 +1,25 @@
+const DocsLayout: React.FC<{
+  LeftSidebarContent: React.FC;
+  RightSidebarContent: React.FC;
+}> = ({ LeftSidebarContent, RightSidebarContent, ...props }) => {
+  return (
+    <div className='max-w-8xl mx-auto px-4 sm:px-6 md:px-8'>
+      <div className='hidden lg:block fixed z-20 inset-0 top-[3.8125rem] left-[max(0px,calc(50%-45rem))] right-auto w-64 pb-10 px-7 overflow-y-auto border-r-2 border-r-slate-200 dark:border-slate-700'>
+        <LeftSidebarContent />
+      </div>
+      <div className='lg:pl-72'>
+        <div className='max-w-3xl mx-auto pt-10 xl:max-w-none xl:ml-0 xl:mr-[15.5rem] xl:pr-16'>
+          <main className='relative z-20 mt-8'>{props.children}</main>
+          <footer className='text-sm leading-6 mt-12'>Footer</footer>
+          <div className='fixed z-20 top-[3.8125rem] bottom-0 right-[max(0px,calc(50%-45rem))] w-[19.5rem] py-10 px-8 overflow-y-auto hidden xl:block'>
+            <div className='overflow-y-auto sticky max-h-[calc(var(--vh)-4rem)]'>
+              <RightSidebarContent />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DocsLayout;
--- pages/[slug]/docs/index.tsx
+++ pages/[slug]/docs/index.tsx
@@ -10,6 +10,7 @@ import getFileContent from '@/utils/getFile';
 import { Remarkable } from 'remarkable';
 // @ts-ignore
 import mdToc from 'markdown-toc';
+import DocsLayout from '@/layouts/DocsLayout';
 // @ts-ignore
 const Page = ({ content, tocHtml, navLinks, navCta, logo, sidebar, slug }) => {
   const Component = useMemo(() => getMDXComponent(content), [content]);
@@ -25,8 +26,8 @@ const Page = ({ content, tocHtml, navLinks, navCta, logo, sidebar, slug }) => {
         <div className='sticky top-0'>
           <DocsNav links={navLinks} navbarCta={navCta} logo={logo} />
         </div>
-        <div className='max-w-8xl mx-auto px-4 sm:px-6 md:px-8'>
-          <div className='hidden lg:block fixed z-20 inset-0 top-[3.8125rem] left-[max(0px,calc(50%-45rem))] right-auto w-64 pb-10 px-7 overflow-y-auto border-r-2 border-r-slate-200 dark:border-slate-700'>
+        <DocsLayout
+          LeftSidebarContent={() => (
             <ul className='space-y-4 mt-10'>
               {sidebar.map((file: string) => {
                 return (
@@ -40,26 +41,19 @@ const Page = ({ content, tocHtml, navLinks, navCta, logo, sidebar, slug }) => {
                 );
               })}
             </ul>
-          </div>
-          <div className='lg:pl-72'>
-            <div className='max-w-3xl mx-auto pt-10 xl:max-w-none xl:ml-0 xl:mr-[15.5rem] xl:pr-16'>
-              <main className='relative z-20 mt-8'>
-                <div className='prose-headings:scroll-mt-20 dark:text-slate-200 dark:prose-headings:text-slate-50 text-slate-800 prose-headings:text-slate-800 prose-headings:font-bold px-3 dark:prose-invert prose-lg prose-slate prose-pre:bg-[#282C34] dark:prose-hr:divide-slate-600 prose-pre:overflow-x-scroll prose-a:text-blue-500 hover:prose-a:bg-blue-100 prose-a:p-[2px] prose-a:rounded-sm dark:prose-a:text-blue-400 dark:hover:prose-a:bg-slate-700 prose-ul:list-disc prose-ol:list-decimal prose-blockquote:border-l-4 prose-blockquote:border-slate-600 prose-blockquote:py-1'>
-                  <Component />
-                </div>
-              </main>
-              <footer className='text-sm leading-6 mt-12'>Footer</footer>
-              <div className='fixed z-20 top-[3.8125rem] bottom-0 right-[max(0px,calc(50%-45rem))] w-[19.5rem] py-10 px-8 overflow-y-auto hidden xl:block'>
-                <div className='overflow-y-auto sticky max-h-[calc(var(--vh)-4rem)]'>
-                  <p className='text-xs mb-8 uppercase'>On this page</p>
-                  <ul
-                    dangerouslySetInnerHTML={{ __html: tocHtml }}
-                    className='space-y-4 prose prose-li:underline-offset-4 text-opacity-50 dark:prose-invert'></ul>
-                </div>
-              </div>
+          )}
+          RightSidebarContent={() => (
+            <div>
+              <p className='text-xs mb-8 uppercase'>On this page</p>
+              <ul
+                dangerouslySetInnerHTML={{ __html: tocHtml }}
+                className='space-y-4 prose prose-li:underline-offset-4 text-opacity-50 dark:prose-invert'></ul>
             </div>
+          )}>
+          <div className='prose-headings:scroll-mt-20 dark:text-slate-200 dark:prose-headings:text-slate-50 text-slate-800 prose-headings:text-slate-800 prose-headings:font-bold px-3 dark:prose-invert prose-lg prose-slate prose-pre:bg-[#282C34] dark:prose-hr:divide-slate-600 prose-pre:overflow-x-scroll prose-a:text-blue-500 hover:prose-a:bg-blue-100 prose-a:p-[2px] prose-a:rounded-sm dark:prose-a:text-blue-400 dark:hover:prose-a:bg-slate-700 prose-ul:list-disc prose-ol:list-decimal prose-blockquote:border-l-4 prose-blockquote:border-slate-600 prose-blockquote:py-1'>
+            <Component />
           </div>
-        </div>
+        </DocsLayout>
       </div>
     </div>
   );
