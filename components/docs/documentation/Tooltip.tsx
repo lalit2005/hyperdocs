4093a659a77ef1a56c84ddd1ead52df2b7d5c033
Lalit <lalitvijay9480@gmail.com>
2022-02-15 12:18:37 +0530

    fix: toolltip component margin

--- components/docs/documentation/Tooltip.tsx
+++ components/docs/documentation/Tooltip.tsx
@@ -2,7 +2,7 @@ import { Tooltip as TooltipComponent } from 'react-tiny-tooltip';
 
 const Tooltip = (props: any) => {
   return (
-    <p className='underline decoration-wavy decoration-blue-400 inline-block cursor-help'>
+    <p className='underline decoration-wavy decoration-blue-400 inline-block cursor-help -my-2'>
       <TooltipComponent {...props}>{props.children}</TooltipComponent>
     </p>
   );
