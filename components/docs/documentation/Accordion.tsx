538a5a77c02d6735bda401174ee66c3705161f4c
Lalit <lalitvijay9480@gmail.com>
2022-02-23 23:04:24 +0530

    refactor: use `<details>` tag for accordions in docs

--- components/docs/documentation/Accordion.tsx
+++ components/docs/documentation/Accordion.tsx
@@ -2,23 +2,12 @@ import * as RadixAccordion from '@radix-ui/react-accordion';
 import { ChevronDown, ChevronRight } from 'react-feather';
 
 export const Accordion = (props: any) => (
-  <RadixAccordion.Root type='multiple' className='not-prose'>
-    {props?.values?.map((value: any, index: number) => (
-      <RadixAccordion.Item
-        key={index}
-        value={index.toString()}
-        className='my-1 w-full rounded-sm bg-slate-200 dark:bg-slate-800'
-      >
-        <div className='px-5 py-2'>
-          <RadixAccordion.Trigger className='flex w-full items-center justify-between text-left'>
-            <p>{value?.title}</p>
-            <ChevronRight />
-          </RadixAccordion.Trigger>
-          <RadixAccordion.Content className='mt-2 text-base text-slate-700 dark:text-slate-400'>
-            {value?.content}
-          </RadixAccordion.Content>
-        </div>
-      </RadixAccordion.Item>
-    ))}
-  </RadixAccordion.Root>
+  <details
+    className='open:ring-1 open:ring-black/5 cursor-pointer hover:bg-slate-100 dark:hover:bg-slate-900 dark:open:ring-white/10 open:shadow-md border border-slate-300 dark:border-slate-700 px-4 py-2 rounded my-1 transition-all'
+    open>
+    <summary className='font-medium select-none'>{props?.title}</summary>
+    <div className='cursor-default'>
+      <p>{props?.children}</p>
+    </div>
+  </details>
 );
