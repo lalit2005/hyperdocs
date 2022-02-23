3c5616e2d3e5f8a1e5bd02261f80fc0e7911abf0
Lalit <lalitvijay9480@gmail.com>
2022-02-23 23:06:53 +0530

    fix: change dashboard navbar bg to black

--- components/DashboardNav.tsx
+++ components/DashboardNav.tsx
@@ -1,13 +1,17 @@
+import clsx from 'clsx';
 import { useSession } from 'next-auth/react';
-import Link from 'next/link';
 import Logo from './Logo';
 import ProfileDropdown from './ProfileDropdown';
 
-const DashboardNav = () => {
+const DashboardNav: React.FC<{ className?: string }> = ({ className }) => {
   const { data } = useSession();
 
   return (
-    <nav className='sticky top-0 z-10 mb-20 bg-white/50 backdrop-blur-sm backdrop-filter dark:bg-slate-900/50'>
+    <nav
+      className={clsx(
+        'sticky top-0 z-10 mb-20 bg-white/50 backdrop-blur-sm backdrop-filter dark:bg-black/50',
+        className
+      )}>
       <div className='flex items-center justify-between px-5'>
         <div>
           <Logo href='/dashboard' />
