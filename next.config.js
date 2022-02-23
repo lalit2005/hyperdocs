7f462d4ea6dd57e6f96b290e4cb5c66d6527e4ff
Lalit <lalitvijay9480@gmail.com>
2022-02-23 23:00:54 +0530

    refactor: redirect /support to new github issue

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/support',
+        destination: 'https://github.com/lalit2005/hyperdocs/issues/new',
+        permanent: false,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
