73f08cde35a3ab4378a7c27e04f35811a5efede4
Lalit <lalitvijay9480@gmail.com>
2022-02-27 22:52:05 +0530

    feat: finish landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import { RoughNotation } from 'react-rough-notation';
 import Link from 'next/link';
 import Image from 'next/image';
 import LandingPageImage from '../public/landing-page-image.png';
-import { Fade } from 'react-awesome-reveal';
+import Hyperdocs3d from '../public/hyperdocs-3d.png';
 import clsx from 'clsx';
 import {
   Zap,
@@ -13,7 +13,12 @@ import {
   Terminal,
   MessageSquare,
   GitHub,
+  Moon,
+  Box,
 } from 'react-feather';
+import WorkflowSvg from '@/components/WorkflowSvg';
+import { Markdown } from '@/components/ui/Typography';
+import { CustomLink } from '@/components/ui/Link';
 
 const Home: NextPage = () => {
   const gradient = 'bg-gradient-to-r text-transparent bg-clip-text';
@@ -25,7 +30,7 @@ const Home: NextPage = () => {
         <p className='mb-2 text-center text-sm font-semibold uppercase text-slate-500 dark:text-slate-400'>
           Open source. Free forever
         </p>
-        <h1 className='text-center text-6xl font-black md:text-7xl'>
+        <h1 className='text-center text-6xl font-black md:text-[80px]'>
           Simplest way to{' '}
           <p className='inline bg-gradient-to-r from-[#40c9ff] to-[#e81cff] bg-clip-text text-transparent'>
             create docs{' '}
@@ -63,21 +68,19 @@ const Home: NextPage = () => {
       </div>
 
       <div className='mt-24'>
-        <Fade direction='up' duration={500}>
-          <div className='max-w-5xl mx-auto'>
-            <Image
-              src={LandingPageImage}
-              alt=''
-              placeholder='blur'
-              className='rounded-lg shadow-xl block'
-            />
-          </div>
-        </Fade>
+        <div className='max-w-5xl mx-auto'>
+          <Image
+            src={LandingPageImage}
+            alt=''
+            placeholder='blur'
+            className='rounded-lg shadow-2xl'
+          />
+        </div>
       </div>
 
       <div className='px-10 max-w-7xl mx-auto mt-40 mb-10'>
         <div>
-          <h2 className='text-5xl font-extrabold'>
+          <h2 className='text-7xl font-extrabold'>
             A feature packed <br />
             <span className={clsx(gradient, `from-[#696eff] to-[#f8acff]`)}>
               documentation generator
@@ -92,7 +95,7 @@ const Home: NextPage = () => {
           {features.map(({ title, description, Icon }) => (
             <div
               key={title}
-              className='p-7 rounded border dark:border-slate-800'>
+              className='p-7 rounded shadow-lg border dark:border-slate-800'>
               <Icon size={25} opacity={0.7} />
               <div className='mt-4'>
                 <h3 className='text-xl font-bold'>{title}</h3>
@@ -103,7 +106,120 @@ const Home: NextPage = () => {
             </div>
           ))}
         </div>
+        <div className='mt-32'>
+          <h3 className='text-7xl font-extrabold max-w-3xl'>
+            <span className='opacity-40 mb-2'>Building docs is boring.</span>
+            <br />
+            <span>
+              But Hyperdocs makes docs{' '}
+              <span
+                className={clsx(
+                  gradient,
+                  'from-cyan-500 to-slate-100 dark:from-[#61f4de] dark:to-slate-900'
+                )}>
+                fun and intuitive
+              </span>
+            </span>
+          </h3>
+          <p className='text-lg opacity-70 mt-2 max-w-3xl'>
+            Just focus on content. Hyperdocs will take care of hosting, feedback
+            collection, blog management, SEO, lighthouse score and many more.{' '}
+            <RoughNotation
+              type='underline'
+              strokeWidth={2}
+              animationDuration={2000}
+              color='rgb(129 140 248)'
+              animate
+              show>
+              <span>Just push to GitHub and it&apos;s done.</span>
+            </RoughNotation>
+          </p>
+          <WorkflowSvg />
+        </div>
       </div>
+      <div className='w-full py-16'>
+        <div className='max-w-7xl mx-auto'>
+          <h3 className='text-7xl font-extrabold'>
+            <span className={clsx(gradient, 'from-[#1dbde6] to-[#f1515e]')}>
+              Prebuilt components{' '}
+            </span>
+            <Box
+              className='inline-block relative -top-1'
+              strokeWidth={3}
+              size={40}
+            />
+          </h3>
+          <p className='mt-3 max-w-3xl text-lg opacity-70'>
+            <Markdown
+              text='Hyperdocs has many prebuilt components that you can just start
+            using right away in your markdown files.'
+            />
+          </p>
+          <div className='grid grid-cols-1 sm:grid-cols-3 md:grid-cols-5 mt-10'>
+            {[
+              'Tooltips',
+              'JSX',
+              'Code playgrounds',
+              'Callouts',
+              'And much more...',
+            ].map((name) => (
+              // TODO: link to docs
+              <div key={name} className='p-4'>
+                <h3 className='text-xl border dark:border-slate-800 font-semibold px-4 py-6 rounded bg-white dark:bg-black text-black dark:text-white h-full flex justify-center items-center text-center'>
+                  {name}
+                </h3>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+      <div className='max-w-7xl mx-auto mt-20'>
+        <h3 className=' font-extrabold text-7xl'>
+          No more{' '}
+          <span
+            className={clsx(
+              gradient,
+              'from-[#6274e7] to-[#8752a3]'
+            )}>{`{ config }`}</span>
+        </h3>
+        <p className='text-lg my-3 opacity-70 max-w-3xl'>
+          Every documentation generator needs a separate <code>`.json`</code>{' '}
+          config file to function properly. Although Hyperdocs works without any
+          config, you can add a <code>`_sidebar.txt`</code> file to customize
+          the order in which links appear in sidebar.
+        </p>
+        {/* // TODO: add image */}
+      </div>
+      <div className='mt-20 max-w-7xl mx-auto text-invert rounded-xl py-10 shadow-xl mb-5'>
+        <h3
+          className={clsx(
+            gradient,
+            'text-7xl font-extrabold from-[#f44369] to-[#3e3b92] text-center'
+          )}>
+          Supercharge your docs now
+        </h3>
+
+        <div className='text-invert max-w-xs mx-auto flex flex-wrap justify-around'>
+          <CustomLink
+            href='/new'
+            className='text-center text-2xl font-semibold !p-4 mt-10 shadow-2xl hover:scale-105 transition-all'>
+            Get started now →
+          </CustomLink>
+        </div>
+      </div>
+      <div className='mt-20 max-w-xl mx-auto'>
+        <Image src={Hyperdocs3d} alt='' className='dark:invert' />
+      </div>
+      <footer className='px-10 py-5 border-t dark:border-slate-700 mt-10'>
+        <div>
+          <a
+            className='text-blue-500 hover:text-blue-700'
+            rel='noopener noreferrer'
+            href='https://lalit2005.hashnode.dev/hyperdocs'>
+            Made by lalit
+          </a>
+        </div>
+      </footer>
     </div>
   );
 };
@@ -120,7 +236,7 @@ const features = [
   {
     title: 'Auto updating',
     description:
-      'Spend a minute on setup and forget the rest. Just keep updating the docs/ folder and the website will auto update automagically.',
+      'Spend a minute on setup and forget the rest. Just keep updating the `docs/` folder and the website will auto update automagically.',
     Icon: Download,
   },
   {
