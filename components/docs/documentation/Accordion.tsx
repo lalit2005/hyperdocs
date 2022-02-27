3bc34985f4d0e0bee57b1b785b303f39b4038ef5
Lalit <lalitvijay9480@gmail.com>
2022-02-28 05:06:24 +0530

    fix: default accordion open state

--- components/docs/documentation/Accordion.tsx
+++ components/docs/documentation/Accordion.tsx
@@ -1,7 +1,5 @@
 export const Accordion = (props: any) => (
-  <details
-    className='open:ring-1 open:ring-black/5 cursor-pointer hover:bg-slate-100 dark:hover:bg-slate-900 dark:open:ring-white/10 open:shadow-md border border-slate-300 dark:border-slate-700 px-4 py-2 rounded my-1 transition-all'
-    open>
+  <details className='open:ring-1 open:ring-black/5 cursor-pointer hover:bg-slate-100 dark:hover:bg-slate-900 dark:open:ring-white/10 open:shadow-md border border-slate-300 dark:border-slate-700 px-4 py-2 rounded my-1 transition-all'>
     <summary className='font-medium select-none'>{props?.title}</summary>
     <div className='cursor-default'>
       <p>{props?.children}</p>
