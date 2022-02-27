04289aa999919748583eb0c68e677ab7e76b044f
Lalit <lalitvijay9480@gmail.com>
2022-02-27 16:44:22 +0530

    feat: create hyperdocs-cli

---
+++ cli/src/constants.ts
@@ -0,0 +1,33 @@
+export const SidebarFileContent = `introduction.md
+getting-started.md
+`;
+
+export const IndexFileContent = `Welcome to Hyperdocs!
+
+This page will be rendered at at the root of the documetation at /docs/
+
+Visit https://hyperdocs.netlify.app/docs/ for more info.
+`;
+
+export const IntroductionFileContent = `# Introduction
+
+Introduce your project here.
+
+You can use jsx inside this markdown file and also use many prebuilt components.
+
+Visit https://hyperdocs.netlify.app/docs/ for more info.
+`;
+
+export const GettingStartedFileContent = `# Getting Started
+
+Write your project's getting started guide here.
+
+To create a new page, just run \`hyperdocs-cli new <filename>\``;
+
+export const NextStepsMessage = `Next steps:
+1. Run \`hyperdocs-cli new <filename>\` to create a new page
+2. Push it to GitHub.
+3. Make sure you setup a docs site in Hyperdocs dashboard.
+4. You can customize the navbar, themes, etc. in the dashboard.
+5. If everything is fine, the documentation site will update automatically in a few mins.
+`;
---
+++ cli/src/index.ts
@@ -0,0 +1,66 @@
+#!/usr/bin/env node
+
+import cac from 'cac';
+import { mkdirSync, writeFileSync, appendFileSync } from 'fs';
+import chalk from 'chalk';
+import * as Constants from './constants';
+
+const cli = cac();
+
+cli
+  .command('init', 'Create a docs/ folder with required files for Hyperdocs')
+  .action(() => {
+    console.log('Initializing docs/ folder...');
+    // create docs/ folder
+    try {
+      mkdirSync('docs');
+    } catch (_) {
+      console.log(
+        chalk.red('Hey, it seems like docs/ folder already exists here!')
+      );
+      process.exit(1);
+    }
+
+    // create index.md
+    writeFileSync('docs/index.md', Constants.SidebarFileContent);
+
+    // create _sidebar.txt
+    writeFileSync('docs/_sidebar.txt', Constants.SidebarFileContent);
+
+    // create introduction.md
+    writeFileSync('docs/introduction.md', Constants.IntroductionFileContent);
+
+    // create getting-started.md
+    writeFileSync(
+      'docs/getting-started.md',
+      Constants.GettingStartedFileContent
+    );
+
+    console.log(chalk.bgGreen.black('Done!'));
+    console.log(chalk.green(Constants.NextStepsMessage));
+  });
+
+cli
+  .command('new <file>', 'Create a new page in docs/ folder')
+  .action((file: string) => {
+    // check if the current directory is docs/
+    if (process.cwd().split('/').pop() === 'docs') {
+      console.log(
+        chalk.red(
+          'Please run this command from a level above docs/ folder to create a new page.'
+        )
+      );
+      process.exit(1);
+    }
+    file = file.endsWith('.md') ? file : `${file}.md`;
+
+    console.log(`Creating ${file}...`);
+    writeFileSync(`docs/${file}`, '# Edit this now \n');
+    appendFileSync(`docs/_sidebar.txt`, `${file}\n`);
+    console.log(chalk.green.bold(`Done!`));
+    console.log(`This page is appended to _sidebar.txt. You can reorder it.`);
+  });
+
+cli.help();
+cli.version('0.0.1');
+cli.parse();
