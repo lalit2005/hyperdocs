4e711e812c2c473d3d606650bb8d767875439947
Lalit <lalitvijay9480@gmail.com>
2022-02-28 18:16:21 +0530

    fix: navbar z-index

--- components/docs/navbar.tsx
+++ components/docs/navbar.tsx
@@ -9,7 +9,7 @@ const DocsNav: React.FC<{
   logo: string;
 }> = ({ links, navbarCta, logo, ...props }) => {
   return (
-    <nav className='flex w-screen items-center justify-between bg-white py-3 px-10 shadow-lg dark:border-b dark:border-b-slate-700 dark:bg-black dark:shadow-slate-800/50 sticky top-0'>
+    <nav className='flex w-screen items-center justify-between bg-white py-3 px-10 shadow-lg dark:border-b dark:border-b-slate-700 dark:bg-black dark:shadow-slate-800/50 sticky top-0 z-50'>
       <div className='text-lg font-bold'>
         <Link href='/'>
           <a>{logo}</a>
