522c5e427009926932bfecb7fc79895f3c368dbb
Lalit <lalitvijay9480@gmail.com>
2022-03-01 15:56:20 +0530

    refactor: add shtr to showcase

--- data/showcase.ts
+++ data/showcase.ts
@@ -18,10 +18,17 @@ const showcaseSites: {
       'https://user-images.githubusercontent.com/69138026/156039285-2ea7e303-c14d-4b31-b079-addbbe494c77.png',
   },
   {
+    name: 'Shtr.',
+    desc: 'Shtr is a powerful, free, open-source, and self hosting url shortner',
+    link: 'https://hyperdocs.netlify.app/shtr/docs',
+    image:
+      'https://thetechhulk.com/_next/image?url=https%3A%2F%2Fcdn.hashnode.com%2Fres%2Fhashnode%2Fimage%2Fupload%2Fv1646075098522%2Fgt2-LXQrY.png%3Fw%3D1600%26h%3D840%26fit%3Dcrop%26crop%3Dentropy%26auto%3Dcompress%2Cformat%26format%3Dwebp&w=1920&q=75',
+  },
+  {
     name: 'Add yours here',
     desc: 'Add your own site to showcase here',
     link: '/docs/showcase',
-    image: 'https://cdn.statically.io/og/+',
+    image: 'https://cdn.statically.io/og/Add%20yours%20here-%3E',
   },
 ];
 
