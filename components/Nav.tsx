aa583979ca87b0c0f80434f46b69212830197ef7
Lalit <lalitvijay9480@gmail.com>
2022-03-01 19:57:52 +0530

    feat: add hamburger menu in nav

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,10 +1,13 @@
 import Link from 'next/link';
 import Logo from './Logo';
 import { useSession } from 'next-auth/react';
-import { Tooltip } from 'react-tiny-tooltip';
+import { Menu } from 'react-feather';
+import { MenuContent, MenuItem, MenuRoot, MenuTrigger } from './ui/Menu';
+import { useState } from 'react';
 
 const Nav = () => {
   const { data, status } = useSession();
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <nav className='sticky top-0 mb-20 bg-white/70 dark:bg-black/70 backdrop-blur-md z-50 shadow-sm'>
       <div className='flex items-center justify-between px-5'>
@@ -12,7 +15,27 @@ const Nav = () => {
           <Logo />
         </div>
         <div>
-          <ul className='space-x-10 py-5'>
+          <div className='block md:hidden'>
+            <MenuRoot isOpen={isOpen} setIsOpen={setIsOpen}>
+              <MenuTrigger>
+                <div>
+                  <Menu className='inline-block p-1 my-4' size={30} />
+                </div>
+                <MenuContent isOpen={isOpen}>
+                  <MenuItem link='/hyperdocs/docs'>Docs</MenuItem>
+                  <MenuItem link='/hyperdocs/showcase'>Showcase</MenuItem>
+                  <MenuItem link='/hyperdocs/blog'>Blog</MenuItem>
+                  <MenuItem link='/login'>Login</MenuItem>
+                  {status === 'unauthenticated' || status === 'loading' ? (
+                    <MenuItem link='/login'>Login</MenuItem>
+                  ) : (
+                    <MenuItem link='/dashboard'>Dashboard</MenuItem>
+                  )}
+                </MenuContent>
+              </MenuTrigger>
+            </MenuRoot>
+          </div>
+          <ul className='space-x-10 py-5 hidden md:block'>
             <Link href='/hyperdocs/docs'>
               <a className='opacity-60 transition-all hover:opacity-100'>
                 Docs
