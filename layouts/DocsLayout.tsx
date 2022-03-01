137db6fad405ccec34887ada65d248b05339a46d
Lalit <lalitvijay9480@gmail.com>
2022-03-01 14:19:07 +0530

    fix: fix responsiveness of docs

--- layouts/DocsLayout.tsx
+++ layouts/DocsLayout.tsx
@@ -14,12 +14,12 @@ const DocsLayout: React.FC<{
   ...props
 }) => {
   return (
-    <div className='max-w-8xl mx-auto px-4 sm:px-6 md:px-8'>
+    <div className='w-screen px-4 sm:px-6 md:px-8'>
       <div className='fixed inset-0 top-[3.8125rem] left-[max(0px,calc(50%-45rem))] right-auto z-20 hidden w-64 overflow-y-auto border-r-2 border-r-slate-200 px-7 pb-10 dark:border-slate-700 lg:block'>
         <LeftSidebarContent />
       </div>
       <div className='lg:pl-72'>
-        <div className='mx-auto max-w-3xl pt-10 xl:ml-0 xl:mr-[15.5rem] xl:max-w-none xl:pr-16'>
+        <div className='mx-auto max-w-3xl pt-10 xl:ml-0 2xl:ml-[max(0px,calc(60%-45rem))]'>
           <main className='relative z-20 mt-8'>{props.children}</main>
           <footer className='py-4 px-3 mt-5 border-t-2 border-t-slate-300 dark:border-t-slate-700'>
             <Markdown text={footerText} />
