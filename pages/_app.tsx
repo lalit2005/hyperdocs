35a29392853722249c0aba03dfd4d373bbdc73e1
Lalit <lalitvijay9480@gmail.com>
2022-03-01 12:50:26 +0530

    feat: seo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,23 +5,44 @@ import { SessionProvider } from 'next-auth/react';
 import { SWRConfig } from 'swr';
 import Head from 'next/head';
 import { Toaster } from 'react-hot-toast';
+import { NextSeo } from 'next-seo';
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
-    <SessionProvider session={session}>
-      <Head>
-        <title>Hyperdocs - Documentation made easy</title>
-        <link rel='shortcut icon' href='/favicon.png' type='image/x-icon' />
-      </Head>
-      <SWRConfig
-        value={{ fetcher: (url) => fetch(url).then((res) => res.json()) }}
-      >
-        <ThemeProvider attribute='class'>
-          <Component {...pageProps} />
-          <Toaster position='bottom-left' />
-        </ThemeProvider>
-      </SWRConfig>
-    </SessionProvider>
+    <div>
+      <NextSeo
+        title='Hyperdocs'
+        description='Easiest way to build your own documentation website without any config'
+        openGraph={{
+          url: 'https://hyperdocs.netlify.app',
+          title: 'Hyperdocs',
+          description:
+            'Easiest way to build your own documentation website without any config',
+          images: [
+            {
+              url: 'https://hyperdocs.netlify.app/ogimage.png',
+            },
+          ],
+        }}
+        twitter={{
+          cardType: 'summary_large_image',
+          handle: '@lalit2005',
+        }}
+      />
+      <SessionProvider session={session}>
+        <Head>
+          <title>Hyperdocs - Documentation made easy</title>
+          <link rel='shortcut icon' href='/favicon.png' type='image/x-icon' />
+        </Head>
+        <SWRConfig
+          value={{ fetcher: (url) => fetch(url).then((res) => res.json()) }}>
+          <ThemeProvider attribute='class'>
+            <Component {...pageProps} />
+            <Toaster position='bottom-left' />
+          </ThemeProvider>
+        </SWRConfig>
+      </SessionProvider>
+    </div>
   );
 }
 
