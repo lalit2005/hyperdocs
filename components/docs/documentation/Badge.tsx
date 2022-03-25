b3ad360111ff6b069b7033081b4f4fde53893f3c
Lalit <lalitvijay9480@gmail.com>
2022-03-25 05:44:37 +0530

    refactor: change contrast of badges

--- components/docs/documentation/Badge.tsx
+++ components/docs/documentation/Badge.tsx
@@ -6,15 +6,15 @@ const Badge = (props: any) => {
       className={clsx(
         'px-2 py-1 rounded border dark:bg-transparent',
         props.color == 'blue' &&
-          'border-blue-200 text-blue-500 bg-blue-100 dark:border-blue-700 dark:text-blue-500',
+          'border-blue-200 text-blue-700 bg-blue-100 dark:border-blue-700 dark:text-blue-500',
         props.color == 'green' &&
-          'border-green-200 text-green-500 bg-green-200 dark:border-green-700 dark:text-green-500',
+          'border-green-200 text-green-700 bg-green-200 dark:border-green-700 dark:text-green-500',
         props.color == 'yellow' &&
-          'border-yellow-200 text-yellow-500 bg-yellow-100 dark:border-yellow-700 dark:text-yellow-500',
+          'border-yellow-200 text-yellow-700 bg-yellow-100 dark:border-yellow-700 dark:text-yellow-500',
         props.color == 'red' &&
-          'border-red-200 text-red-500 bg-red-100 dark:border-red-700 dark:text-red-500',
+          'border-red-200 text-red-700 bg-red-100 dark:border-red-700 dark:text-red-500',
         !props.color &&
-          'border-green-200 text-green-500 bg-green-200 dark:border-green-700 dark:text-green-500'
+          'border-green-200 text-green-700 bg-green-200 dark:border-green-700 dark:text-green-500'
       )}>
       {props?.children}
     </span>
