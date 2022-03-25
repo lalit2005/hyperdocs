4181b1536ad4c859d49b469c0b881d247b6cfa92
Lalit <lalitvijay9480@gmail.com>
2022-03-25 05:38:03 +0530

    refactor: change badge styles

--- components/docs/documentation/Badge.tsx
+++ components/docs/documentation/Badge.tsx
@@ -4,17 +4,17 @@ const Badge = (props: any) => {
   return (
     <span
       className={clsx(
-        'px-2 py-1 rounded border',
+        'px-2 py-1 rounded border dark:bg-transparent',
         props.color == 'blue' &&
-          'border-blue-200 text-blue-500 bg-blue-100 dark:border-blue-900 dark:text-blue-200 dark:bg-blue-700',
+          'border-blue-200 text-blue-500 bg-blue-100 dark:border-blue-700 dark:text-blue-500',
         props.color == 'green' &&
-          'border-green-200 text-green-500 bg-green-200 dark:border-green-900 dark:text-green-200 dark:bg-green-700',
+          'border-green-200 text-green-500 bg-green-200 dark:border-green-700 dark:text-green-500',
         props.color == 'yellow' &&
-          'border-yellow-200 text-yellow-500 bg-yellow-100 dar:border-yellow-900 dark:text-yellow-200 dark:bg-yellow-700',
+          'border-yellow-200 text-yellow-500 bg-yellow-100 dark:border-yellow-700 dark:text-yellow-500',
         props.color == 'red' &&
-          'border-red-200 text-red-500 bg-red-100 dark:border-red-900 dark:text-red-200 dark:bg-red-700',
+          'border-red-200 text-red-500 bg-red-100 dark:border-red-700 dark:text-red-500',
         !props.color &&
-          'border-green-200 text-green-500 bg-green-200 dark:border-green-900 dark:text-green-200 dark:bg-green-700'
+          'border-green-200 text-green-500 bg-green-200 dark:border-green-700 dark:text-green-500'
       )}>
       {props?.children}
     </span>
