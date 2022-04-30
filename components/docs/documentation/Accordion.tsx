6f974edfc81f46ab19b15e8f692100a13325b23c
Lalit <lalitvijay9480@gmail.com>
2022-05-01 00:00:04 +0530

    refactor: remove shadow from accordions

--- components/docs/documentation/Accordion.tsx
+++ components/docs/documentation/Accordion.tsx
@@ -1,5 +1,5 @@
 export const Accordion = (props: any) => (
-  <details className='my-1 cursor-pointer rounded border border-gray-300 px-4 py-2 transition-all open:shadow-md dark:border-gray-700'>
+  <details className='my-1 cursor-pointer rounded border border-gray-300 px-4 py-2 transition-all dark:border-gray-700'>
     <summary className='select-none font-medium'>{props?.title}</summary>
     <div className='cursor-default'>
       <p>{props?.children}</p>
