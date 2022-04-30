a85b70a5e19c519824ab6fd6804660c4e2eb15f9
Lalit <lalitvijay9480@gmail.com>
2022-04-30 23:59:13 +0530

    refactor: update callouts

--- components/docs/documentation/Callout.tsx
+++ components/docs/documentation/Callout.tsx
@@ -5,16 +5,24 @@ const Callout = (props: any) => {
   return (
     <div
       className={clsx(
-        'not-prose callout my-5 rounded p-7 text-black',
-        props.type == 'info' && 'bg-blue-400',
-        props.type == 'tip' && 'bg-cyan-400',
-        props.type == 'success' && 'bg-green-400',
-        props.type == 'warning' && 'bg-yellow-400',
-        props.type == 'danger' && 'bg-red-400',
-        !props.type && 'bg-blue-400'
-      )}
-    >
-      <div className='mb-3'>
+        'not-prose callout my-5 border-l-4 p-7',
+        props.type == 'info' && 'border-blue-500 bg-gray-800/70',
+        props.type == 'tip' && 'border-cyan-500 bg-gray-800/70',
+        props.type == 'success' && 'border-green-500 bg-gray-800/70',
+        props.type == 'warning' && 'border-yellow-500 bg-gray-800/70',
+        props.type == 'danger' && 'border-red-500 bg-gray-800/70',
+        !props.type && 'border-blue-500 bg-gray-800/70'
+      )}>
+      <div
+        className={clsx(
+          'mb-3',
+          props.type == 'info' && 'text-blue-500',
+          props.type == 'tip' && 'text-cyan-500',
+          props.type == 'success' && 'text-green-500',
+          props.type == 'warning' && 'text-yellow-500',
+          props.type == 'danger' && 'text-red-500',
+          !props.type && 'text-blue-500'
+        )}>
         {(props.type == 'info' && <FeatherIcons.Info />) ||
           (props.type == 'tip' && <FeatherIcons.Zap />) ||
           (props.type == 'success' && <FeatherIcons.CheckCircle />) ||
